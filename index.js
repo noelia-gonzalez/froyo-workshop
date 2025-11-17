e19a56cc5eb6d140bce330bf19c01ab6e26da35c
Noelia Gonzalez <noeliag2210@gmail.com>
2025-11-16 20:48:40 -0600

    prompted the user to input froyo flavors

---
+++ index.js
@@ -0,0 +1,18 @@
+//prompt the user for froyo flavors with them being separated by commas
+const froyoFlavor = prompt(
+  `Please enter a list of froyo flavors, separated by commas:`
+);
+
+//split user input string into an array of strings
+
+//use loop to iterate through the array of flavors
+
+//use an object to keep count of how many orders there are of each flavor
+
+//use a function to keep the counting of frequencies of elements in an array
+// to stay organized and to make sure that an object is returned
+
+/* NOTE:
+Variables and functions should have meaningful names. 
+Console outputs should change depending on the user's input
+*/
