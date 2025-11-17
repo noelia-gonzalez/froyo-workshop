fa317fd347d9f118aa7af62f40665ba503f70e4a
Noelia Gonzalez <noeliag2210@gmail.com>
2025-11-16 21:35:04 -0600

    finished everything in this branch

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-//prompt the user for froyo flavors with them being separated by commas
-const froyoFlavor = prompt(
-  `Please enter a list of froyo flavors, separated by commas:`
-);
-
-//split user input string into an array of strings
-
-//use loop to iterate through the array of flavors
-
-//use an object to keep count of how many orders there are of each flavor
-
 //use a function to keep the counting of frequencies of elements in an array
-// to stay organized and to make sure that an object is returned
+const trackFroyoFlavors = () => {
+  //prompt the user for froyo flavors with them being separated by commas
+  const userInput = prompt(
+    `Please enter a list of froyo flavors, separated by commas:`
+  );
+  //split user input string into an array of strings
+  const froyoFlavors = userInput.split(",");
+  //.map creates a new array, .trim takes out unneccessary spaces(i looked this up)
+  const newFlavors = froyoFlavors.map((froyo) => froyo.trim());
+  //use an object to keep count of how many orders there are of each flavor
+  const userFroyoCount = {};
+  //use loop to iterate through the array of flavors
+  for (const key in newFlavors) {
+    const flavor = newFlavors[key];
+
+    if (userFroyoCount[flavor]) {
+      userFroyoCount[flavor] += 1;
+    } else {
+      userFroyoCount[flavor] = 1;
+    }
+  }
+  console.log(userFroyoCount);
+};
+
+trackFroyoFlavors();
 
 /* NOTE:
 Variables and functions should have meaningful names. 
 Console outputs should change depending on the user's input
+
+USE THIS TO TEST: vanilla, vanilla, vanilla, strawberry, coffee, coffee
 */
